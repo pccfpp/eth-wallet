23591c17c8bda4b45278f7c409f183e8bf62b90c
Filippo Piccardi <filippo_piccardi@yahoo.it>
2017-12-03 23:28:57 +0100

    add get routes

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,14 @@
 import { version } from '../../package.json';
 import { Router } from 'express';
 import facets from './facets';
+import Web3 from 'web3';
 
 export default ({ config, db }) => {
 	let api = Router();
+
+	var web3 = new Web3(
+		new Web3.providers.HttpProvider('https://rinkeby.infura.io/')
+	);
 
 	// mount the facets resource
 	api.use('/facets', facets({ config, db }));
@@ -11,6 +16,36 @@ export default ({ config, db }) => {
 	// perhaps expose some API metadata at the root
 	api.get('/', (req, res) => {
 		res.json({ version });
+	});
+
+	api.get('/createWallet', (req, res) => {
+		var wallet = web3.eth.accounts.create();
+		var walletMin = {
+			address: wallet.address,
+			privateKey: wallet.privateKey
+		};
+		res.json(walletMin);
+	});
+
+	api.get('/getBalance/:param', (req, res) => {
+		var address = req.params.param;
+		web3.eth.getBalance(address)
+			.then(balance => res.json(web3.utils.fromWei(balance))).catch(err => res.json(err))
+	});
+
+	api.post('/transaction', (req, res) => {
+	//transaction initiation
+		const txParams = {
+			nonce: web3.eth,
+			gasPrice: '0x09184e72a000', 
+			gasLimit: '0x2710',
+			to: '0x0000000000000000000000000000000000000000', 
+			value: '0x00', 
+		  }
+		const tx = new EthereumTx(txParams)
+		tx.sign(privateKey)
+		const serializedTx = tx.serialize()
+		
 	});
 
 	return api;
