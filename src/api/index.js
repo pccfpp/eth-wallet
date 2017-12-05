e5f570b20114d2e492b939466130f6e552bdc183
Filippo Piccardi <filippo_piccardi@yahoo.it>
2017-12-05 04:03:48 +0100

    added last route

--- src/api/index.js
+++ src/api/index.js
@@ -2,14 +2,15 @@ import { version } from '../../package.json';
 import { Router } from 'express';
 import facets from './facets';
 import Web3 from 'web3';
+import Tx from 'ethereumjs-tx'; 
 
 export default ({ config, db }) => {
 	let api = Router();
-
-	var web3 = new Web3(
-		new Web3.providers.HttpProvider('https://rinkeby.infura.io/')
+	
+	var web3 = new Web3(                                                //connect to a remote node to 
+		new Web3.providers.HttpProvider('https://rinkeby.infura.io/')   //communicate with the ethereum network
 	);
-
+	
 	// mount the facets resource
 	api.use('/facets', facets({ config, db }));
 
@@ -19,34 +20,51 @@ export default ({ config, db }) => {
 	});
 
 	api.get('/createWallet', (req, res) => {
-		var wallet = web3.eth.accounts.create();
-		var walletMin = {
+		var account = web3.eth.accounts.create();      //create an account from the web3 boilerplate
+		var wallet = {                                 //edit it to our purpose
 			address: wallet.address,
 			privateKey: wallet.privateKey
 		};
-		res.json(walletMin);
+		res.json(myWallet);
 	});
 
 	api.get('/getBalance/:param', (req, res) => {
 		var address = req.params.param;
 		web3.eth.getBalance(address)
-			.then(balance => res.json(web3.utils.fromWei(balance))).catch(err => res.json(err))
+			.then(balance => res.json(web3.utils.fromWei(balance)))		//render the balance in the right format
+			.catch(err => res.json(err))
 	});
 
 	api.post('/transaction', (req, res) => {
-	//transaction initiation
-		const txParams = {
-			nonce: web3.eth,
-			gasPrice: '0x09184e72a000', 
-			gasLimit: '0x2710',
-			to: '0x0000000000000000000000000000000000000000', 
-			value: '0x00', 
-		  }
-		const tx = new EthereumTx(txParams)
-		tx.sign(privateKey)
-		const serializedTx = tx.serialize()
-		
+	
+	var privateKey = req.body.privateKey;                                       //retrieve necessary transaction data 
+	var destination = req.body.destination;										//from the body
+	var amount = req.body.amount;
+	var account = web3.eth.accounts.privateKeyToAccount(req.body.privateKey);
+	
+	//initializing raw transaction
+	var rawTx = {
+		nonce: web3.eth.getTransactionCount(account.address),
+		gas: gas,
+		gasPrice: '0x09184e72a000',
+		gasLimit: '0x2710',
+		to: destination,
+		value: amount,
+		from: account.address
+	};
+		    
+	var tx = new Tx(rawTx);               //creates a new instance of the transaction
+	tx.sign(privateKey);                  //sign the transaction
+	var serializedTx = tx.serialize();    //serialize the transaction
+			
+	web3.eth.sendSignedTransaction('0x' + serializedTx.toString('hex'))   //send the transaction
+		.on('receipt', (res) => {
+			res.json(res);
+		})
+		.catch(err => {
+			res.json(err);
+		});
 	});
 
 	return api;
-}
+};
